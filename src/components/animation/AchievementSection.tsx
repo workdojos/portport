373a9f4302bc0e3041d3fc3ee1dde85913dcaeef
WorkDojos <36578874+workdojos@users.noreply.github.com>
2024-11-25 19:22:36 -0600

    as

--- src/components/animation/AchievementSection.tsx
+++ src/components/animation/AchievementSection.tsx
@@ -5,58 +5,57 @@ import { AchievementProps } from "@/types";
 import CountUp from "react-countup";
 
 const achievementsList: AchievementProps[] = [
-  {
-    metric: "sfg sf gsfdg sdfg",
-    value: "25",
-    postfix: "*",
-  },
-  {
-    metric: "sdfg sdfg  fgsfgs fg",
-    value: "sfg sfg sfdg sdfg sdfg",
-    postfix: "*",
-  },
-  {
-    metric: "sdfg sdfg sfdg sdfg sfd",
-    value: "sd gfsdfg sdf gsfd gsd",
-    postfix: "*",
-  },
-  {
-    metric: "sdf gsdf ggfdsfgs fgfsdgsf",
-    value: "sdfg sd fgfsdg sdfg sd",
-    postfix: "*",
-  }
+  {
+    metric: "Startups and founders on-board",
+    value: "15",
+    postfix: "*",
+  },
+  {
+    metric: "Fundable pitch decks & and one-pagers",
+    value: "15",
+    postfix: "*",
+  },
+  {
+    metric: "Open-source deployments",
+    value: "500",
+    postfix: "+",
+  },
+  {
+    metric: "VC capital",
+    value: "0",
+    postfix: ".",
+  }
 ];
 
 const AchievementsSection = () => {
-  return (
-    <section className="pt-4 pb-12 xl:pt-24 xl:pb-0">
-      <div className="primary-bd container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
-          {achievementsList.map((achievement, index) => {
-              return (
-              <div
-                key={index}
-                className="flex flex-1 items-center gap-4 justify-center xl:justify-start"
-              >
-                <CountUp 
-                  end={parseInt(achievement.value+achievement.postfix)}
-                  duration={5}
-                  delay={2}
-                className="text-4xl text-outline text-transparent xl:text-6xl font-extrabold"
-                />
-                <p
-                  className={`text-white/80 ${achievement.metric.length > 15 ? 'max-w-[100px]' : 'max-w-[150px]'} leading-snug`}
-                >
-                  {achievement.metric}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </section>
-  );
+  return (
+    <section className="pt-4 pb-12 xl:pt-24 xl:pb-0">
+      <div className="primary-bd container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {achievementsList.map((achievement, index) => {
+              return (
+              <div
+                key={index}
+                className="flex flex-1 items-center gap-4 justify-center xl:justify-start"
+              >
+                <CountUp 
+                  end={parseInt(achievement.value+achievement.postfix)}
+                  duration={5}
+                  delay={2}
+                className="text-4xl text-outline text-transparent xl:text-6xl font-extrabold"
+                />
+                <p
+                  className={`text-white/80 ${achievement.metric.length > 15 ? 'max-w-[100px]' : 'max-w-[150px]'} leading-snug`}
+                >
+                  {achievement.metric}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AchievementsSection;
-
